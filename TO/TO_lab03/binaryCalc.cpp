a57dc3b2ecfd39d3961e0aafab0869c14ace7ebb
YOSET C <64040965+ycozco@users.noreply.github.com>
2023-10-02 16:45:25 +0000

    calc update comentarios

--- TO/TO_lab03/binaryCalc.cpp
+++ TO/TO_lab03/binaryCalc.cpp
@@ -7,31 +7,40 @@
 // Clase base para los nodos del árbol
 class Node {
 public:
+    // Método virtual para evaluar el nodo
     virtual double evaluate() = 0;
+    // Destructor virtual
     virtual ~Node() {}
 };
 
+// Clase para representar números (nodos hoja)
 class NumberNode : public Node {
-    double value;
+    double value; // Valor del número
 public:
+    // Constructor que inicializa el valor del nodo
     NumberNode(double v) : value(v) {}
+    // Evaluar el nodo devuelve su valor
     double evaluate() override {
         return value;
     }
 };
 
+// Clase base para operadores binarios (+, -, *, /)
 class BinaryOperatorNode : public Node {
 protected:
-    Node* left;
-    Node* right;
+    Node* left;  // Nodo hijo izquierdo
+    Node* right; // Nodo hijo derecho
 public:
+    // Constructor que inicializa los nodos hijos
     BinaryOperatorNode(Node* l, Node* r) : left(l), right(r) {}
+    // Destructor que libera la memoria de los nodos hijos
     ~BinaryOperatorNode() {
         delete left;
         delete right;
     }
 };
 
+// Clases para representar cada operador binario
 class SumNode : public BinaryOperatorNode {
 public:
     using BinaryOperatorNode::BinaryOperatorNode;
@@ -64,26 +73,8 @@ public:
     }
 };
 
-// Funciones auxiliares para tokenización y construcción del árbol
-std::vector<std::string> tokenize(const std::string& s);
-std::vector<std::string> toPostfix(const std::vector<std::string>& tokens);
-Node* constructTree(const std::vector<std::string>& postfix);
-
-int main() {
-    std::string expression;
-    std::cout << "Introduce una expresión matemática: ";
-    std::getline(std::cin, expression);
-
-    auto tokens = tokenize(expression);
-    auto postfix = toPostfix(tokens);
-    Node* tree = constructTree(postfix);
-
-    std::cout << "Resultado: " << tree->evaluate() << std::endl;
-
-    delete tree;
-    return 0;
-}
-
+// Función para tokenizar la expresión
+// Convierte la expresión en una lista de tokens (números, operadores, paréntesis)
 std::vector<std::string> tokenize(const std::string& s) {
     std::vector<std::string> tokens;
     std::string token;
@@ -104,12 +95,15 @@ std::vector<std::string> tokenize(const std::string& s) {
     return tokens;
 }
 
+// Función para determinar la precedencia de los operadores
 int precedence(const std::string& op) {
     if (op == "+" || op == "-") return 1;
     if (op == "*" || op == "/") return 2;
     return 0;
 }
 
+// Función para convertir la lista de tokens a notación postfija (Notación Polaca Inversa)
+// Esta notación facilita la construcción del árbol binario de expresiones
 std::vector<std::string> toPostfix(const std::vector<std::string>& tokens) {
     std::vector<std::string> postfix;
     std::stack<std::string> ops;
@@ -139,6 +133,7 @@ std::vector<std::string> toPostfix(const std::vector<std::string>& tokens) {
     return postfix;
 }
 
+// Función para construir el árbol binario de expresiones a partir de la notación postfija
 Node* constructTree(const std::vector<std::string>& postfix) {
     std::stack<Node*> stack;
     for (const auto& token : postfix) {
@@ -154,4 +149,24 @@ Node* constructTree(const std::vector<std::string>& postfix) {
         }
     }
     return stack.top();
+}
+
+int main() {
+    std::string expression;
+    std::cout << "Introduce una expresión matemática: ";
+    std::getline(std::cin, expression);
+
+    // Tokenizar la expresión
+    auto tokens = tokenize(expression);
+    // Convertir a notación postfija
+    auto postfix = toPostfix(tokens);
+    // Construir el árbol binario de expresiones
+    Node* tree = constructTree(postfix);
+
+    // Evaluar y mostrar el resultado
+    std::cout << "Resultado: " << tree->evaluate() << std::endl;
+
+    // Liberar memoria
+    delete tree;
+    return 0;
 }
